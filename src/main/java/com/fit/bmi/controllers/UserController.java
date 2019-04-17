4dbea380957f2fcf25a4958ca7c82d0be75176c8
thanos loukeris <thanosloukeris@gmail.com>
2019-04-17 15:52:14 +0300

    + audit model + Dto converter and Dto + Cruduserservices

--- src/main/java/com/fit/bmi/controllers/UserController.java
+++ src/main/java/com/fit/bmi/controllers/UserController.java
@@ -1,8 +1,11 @@
 package com.fit.bmi.controllers;
 
 import com.fit.bmi.models.User;
+import com.fit.bmi.payload.DTOconverter;
+import com.fit.bmi.payload.UserDTO;
 import com.fit.bmi.repositories.UserRepository;
 import com.fit.bmi.services.BmiCalculatorImpl;
+import com.fit.bmi.services.CRUDUserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,26 +17,35 @@ import java.util.List;
 @RequestMapping("/api")
 public class UserController {
 
-
-
-    private UserRepository userRepository;
+    private CRUDUserServices crudUserServices;
     private BmiCalculatorImpl bmiCalculator;
+    private DTOconverter dtOconverter;
 
-
-    @Autowired
-    public UserController(UserRepository userRepository, BmiCalculatorImpl bmiCalculator) {
-        this.userRepository = userRepository;
+    public UserController(CRUDUserServices crudUserServices, BmiCalculatorImpl bmiCalculator, DTOconverter dtOconverter) {
+        this.crudUserServices = crudUserServices;
         this.bmiCalculator = bmiCalculator;
+        this.dtOconverter = dtOconverter;
     }
 
     @PostMapping("/register")
-    public User registerUser(@Valid @RequestBody User user){
-        return userRepository.save(user);
-
+    public User registerUser(@Valid @RequestBody UserDTO newUserDTO) {
+        return crudUserServices.registerUser(dtOconverter.convertDTOtoUser(newUserDTO));
     }
 
     @GetMapping("/users")
-    public List<User> getUsers(){
+    public List<User> getUsers() {
         return bmiCalculator.getUsers();
     }
+
+    //Update user by id
+    @PutMapping("/user/{userId}")
+    public User updateUser(@PathVariable Long userId, @Valid @RequestBody UserDTO updateUserDTO) {
+        return crudUserServices.updateUser(dtOconverter.convertDTOtoUser(updateUserDTO), userId);
+    }
+
+    @DeleteMapping("/user/{userId}")
+    void deleteEmployee(@PathVariable Long userId) {
+        crudUserServices.deleteUser(userId);
+    }
+
 }
---
+++ src/main/java/com/fit/bmi/payload/DTOconverter.java
@@ -0,0 +1,19 @@
+package com.fit.bmi.payload;
+
+import com.fit.bmi.models.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DTOconverter {
+
+    public User convertDTOtoUser(UserDTO userDTO){
+        User convertedUser = new User();
+        convertedUser.setHeight(userDTO.getHeight());
+        convertedUser.setName(userDTO.getName());
+        convertedUser.setAge(userDTO.getAge());
+        convertedUser.setWeight(userDTO.getWeight());
+        convertedUser.setLastName(userDTO.getLastName());
+
+        return convertedUser;
+    }
+}
